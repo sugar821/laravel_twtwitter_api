a25a14b75c1aa8801db7b35341c2f63e899ea3eb
sugar821 <aug.sugar@gmail.com>
2019-11-06 12:40:19 +0900

    still

--- database/migrations/2019_10_11_092655_create_tweets_table.php
+++ database/migrations/2019_10_11_092655_create_tweets_table.php
@@ -12,38 +12,38 @@ class CreateTweetsTable extends Migration
      * @return void
      */
     public function up()
-    {
-        Schema::create('tweets', function (Blueprint $table) {
-            $table->string('tweet_id')->unsigned()->unique();
-            $table->text('tweet_avater');
-            $table->text('tweet_user');
-            $table->string('tweet_body');
-            $table->timestamps();
-        });
-    }
-
-    // public function up()
     // {
-    //     switch ($_SERVER['SERVER_NAME'] ?? '127.0.0.1') {
-    //     case '127.0.0.1':
-    //         Schema::create('tweets', function (Blueprint $table) {
+    //     Schema::create('tweets', function (Blueprint $table) {
     //         $table->integer('tweet_id')->unsigned()->unique();
     //         $table->text('tweet_avater');
     //         $table->text('tweet_user');
     //         $table->string('tweet_body');
     //         $table->timestamps();
-    //         });
-    //         break;
-    //     default:
-    //         Schema::create('tweets', function (Blueprint $table) {
-    //         $table->bigint('tweet_id')->unsigned()->unique();
-    //         $table->text('tweet_avater');
-    //         $table->text('tweet_user');
-    //         $table->string('tweet_body');
-    //         $table->timestamps();
     //     });
-    //     }
     // }
+
+    public function up()
+    {
+        switch ($_SERVER['SERVER_NAME'] ?? '127.0.0.1') {
+        case '127.0.0.1':
+            Schema::create('tweets', function (Blueprint $table) {
+            $table->integer('tweet_id')->unsigned()->unique();
+            $table->text('tweet_avater');
+            $table->text('tweet_user');
+            $table->string('tweet_body');
+            $table->timestamps();
+            });
+            break;
+        default:
+            Schema::create('tweets', function (Blueprint $table) {
+            $table->bigint('tweet_id')->unsigned()->unique();
+            $table->text('tweet_avater');
+            $table->text('tweet_user');
+            $table->string('tweet_body');
+            $table->timestamps();
+        });
+        }
+    }
 
     /**
      * Reverse the migrations.
